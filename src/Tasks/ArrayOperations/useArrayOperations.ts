ce78e5fcd34bc4091ddf5ab274df4cb998c3ce2b
vasilisk94 <t.vasilchenko@horo.school>
2023-03-16 23:19:33 +0700

    Fix build (add product type)

--- src/Tasks/ArrayOperations/useArrayOperations.ts
+++ src/Tasks/ArrayOperations/useArrayOperations.ts
@@ -4,8 +4,8 @@ import { useState } from "react";
 type Category = '' // fruit, vegatable, drink
 
 type Product = {
-  // name
-  // price 1.2, 1.5 => Math.round()
+  name: string,
+  price: number,
   // category
 }
 
